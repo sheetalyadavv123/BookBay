1d6bb16959adcf2caa84f04c092a7faf72b8d17c
sheetalyadavv123 <sheetalyad7@gmail.com>
2026-01-25 02:16:14 +0530

    database

--- frontend/src/components/FreeBook.jsx
+++ frontend/src/components/FreeBook.jsx
@@ -1,13 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
-import list from "../../public/list.json"
 import Cards from "./Cards";
+import axios from 'axios';
 
 
 function FreeBook() {
-    const filterData=list.filter((data)=>data.category==="Free");
+    const[book,setBook]=useState([])
+   useEffect(()=>{
+       const getBook=async()=>{
+         try{
+            const res=await axios.get("http://localhost:4001/book");
+            const data=res.data.filter((data)=>data.category==="Free");
+            console.log(data)
+            setBook(data);
+            
+         }catch(error){
+            console.log(error)
+         }
+       }
+       getBook();
+   },[])
+    
     var settings = {
     dots: true,
     infinite: false,
@@ -52,7 +67,7 @@ function FreeBook() {
      </div>
       <div className="slider-wrapper mt-8">
        <Slider {...settings}>
-      {filterData.map((item) => (
+      {book.map((item) => (
         <Cards item={item} key={item.id} />
       ))}
     </Slider>
